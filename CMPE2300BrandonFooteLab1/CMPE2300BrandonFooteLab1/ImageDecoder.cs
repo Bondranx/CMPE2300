4e2348a1e664bdd6d278e2af552970692fe575a2
Brandon Foote <brandon.foote@gmail.com>
2014-09-04 12:36:10 -0600

    Revert "Revert "Started Lab 1""
    This reverts commit 7943c5610ed4bd989c6b60e27273c8f2fc629d99.

---
+++ CMPE2300BrandonFooteLab1/CMPE2300BrandonFooteLab1/ImageDecoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPE2300BrandonFooteLab1
+{
+    public partial class ImageDecoder : Form
+    {
+        public ImageDecoder()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageTSB_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDLG.ShowDialog() == DialogResult.OK)
+            {
+                PicBoxNew.Load(OpenFileDLG.FileName);
+            }
+        }
+
+        private void DecodeImageTSB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
--- CMPE2300BrandonFooteLab1/CMPE2300BrandonFooteLab1/ImageDecoder.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class ImageDecoder
-
-End Class
