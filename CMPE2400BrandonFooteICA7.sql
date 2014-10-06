000bb939623461b55c0669352757a762ca0187c0
Brandon Foote <brandon.foote@gmail.com>
2014-10-06 08:24:10 -0600

    asd
    sadf

--- CMPE2400BrandonFooteICA7.sql
+++ CMPE2400BrandonFooteICA7.sql
@@ -40,3 +40,82 @@ as
 													go
 
 exec CustNameID
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'CustNameIDbyCountry'
+	)
+	drop procedure CustNameIDbyCountry
+go
+
+create proc CustNameIDbyCountry
+as
+	select	FirstName + ' ' + LastName as 'Customer Name',
+			CustomerID as 'ID'
+	from AdventureWorksLT.SalesLT.Customer
+	where SalesPerson = 'adventure-works\Garrett1' and CustomerID in (select CustomerID
+													from AdventureWorksLT.SalesLT.CustomerAddress
+													where AddressType = 'Main Office' and AddressID in (select AddressID
+																										from AdventureWorksLT.SalesLT.Address
+																										where CountryRegion = 'Canada'))
+																										go
+
+exec CustNameIDbyCountry
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'ProductList'
+	)
+	drop procedure ProductList
+go
+
+create proc ProductList
+@ProductName varchar(50) output,
+@Profitmargin int output
+as
+	select Name, ProductNumber as 'Product Number', ProductID as 'ID'
+	from AdventureWorksLT.SalesLT.Product
+	where Name = @ProductName AND @ProfitMargin >= (ListPrice-StandardCost) 
+	go
+
+exec ProductList 'Chain', 1000
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'ProductListParameters'
+	)
+	drop procedure ProductListParameters
+go
+
+create proc ProductListParameters
+@UserCountry varchar(50) output,
+@UserProductQty int output,
+@UserListPrice int output
+as
+	select ProductNumber as 'Product Number'
+	from AdventureWorksLT.SalesLT.Product
+	where ListPrice >= @UserListPrice AND ProductID in (select ProductID
+				from AdventureWorksLT.SalesLT.SalesOrderDetail
+				where OrderQty >= @UserProductQty AND SalesOrderID in (Select SalesOrderID
+						from AdventureWorksLT.SalesLT.SalesOrderHeader
+						where CustomerID in (select CustomerID
+								from AdventureWorksLT.SalesLT.Customer
+								where CustomerID in (Select CustomerID
+										from AdventureWorksLT.SalesLT.Customer
+										where CustomerID in (select CustomerID
+												from AdventureWorksLT.SalesLT.CustomerAddress
+												where AddressID in (select AddressID
+														from AdventureWorksLT.SalesLT.Address
+														where CountryRegion = @UserCountry))))))
+	go
+
+exec ProductListParameters 'Canada', 1, 1
+go
