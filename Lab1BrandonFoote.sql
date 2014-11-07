714e33319871b684d87d21da2b01989191e405bc
Brandon Foote <brandon.foote@gmail.com>
2014-11-07 13:14:55 -0700

    CMPE2400
    Lab 1 complete

--- Lab1BrandonFoote.sql
+++ Lab1BrandonFoote.sql
@@ -79,6 +79,126 @@ select	SupplierID	as 'Supplier ID',
 		Country,
 		PostalCode	as 'Postal Code'
 from NorthwindTraders.dbo.Suppliers
-
-where (select ProductID
+where @Product in (select ProductID
 	   from NorthwindTraders.dbo.Products
+	   where ProductID=@Product and Products.SupplierID=Suppliers.SupplierID)
+go
+
+exec GetSupplierForProduct 4
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'GetCustomers'
+	)
+drop procedure GetCustomers
+go
+
+create proc GetCustomers
+as
+select	CustomerID as 'Customer ID',
+		CompanyName as 'Customer Name'
+from NorthwindTraders.dbo.Customers
+order by CompanyName asc
+go
+
+exec GetCustomers
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'GetCustomerInfo'
+	)
+drop procedure GetCustomerInfo
+go
+
+create proc GetCustomerInfo
+@Customer as nchar(5) = null
+as
+if(@Customer is not null)
+begin
+select	CompanyName as 'Customer Name',
+		ContactName as 'Customer Contact',
+		ContactTitle as 'Contact Title',
+		Phone as 'Phone Number',
+		Fax as 'Fax Number',
+		Address as 'Customer Address',
+		City as 'City',
+		Region as 'Region',
+		Country as 'Country',
+		PostalCode as 'Postal Code'
+from NorthwindTraders.dbo.Customers
+where @Customer=CustomerID
+end
+else
+return 1
+go
+
+exec GetCustomerInfo 'ALFKI'
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'GetCustomerOrders'
+	)
+drop procedure GetCustomerOrders
+go
+
+create proc GetCustomerOrders
+@Customer as nchar(5) = null
+as
+if(@Customer is not null)
+begin
+select	OrderID as 'Order ID',
+		OrderDate as 'Order Date',
+		RequiredDate as 'Required Date',
+		ShippedDate as 'Shipped Date',
+		E.FirstName + ' ' + E.LastName as 'Processing Employee',
+		O.EmployeeID as 'Employee ID',
+		S.CompanyName as "Shipper's Name",
+		DATEDIFF(day,RequiredDate,ShippedDate) as 'Days Overdue'
+from NorthwindTraders.dbo.Orders as O
+inner join NorthwindTraders.dbo.Employees as E
+on E.EmployeeID=O.EmployeeID
+inner join NorthwindTraders.dbo.Shippers as S
+on S.ShipperID=O.ShipVia
+where @Customer=CustomerID
+end
+else
+return 1
+go
+
+exec GetCustomerOrders 'ALFKI'
+go
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'GetOrderDetails'
+	)
+drop procedure GetOrderDetails
+go
+
+create proc GetOrderDetails
+@Order as integer
+as
+select	P.ProductName as 'Product Name',
+		OD.UnitPrice as 'Unit Price',
+		OD.Quantity as 'Quantity',
+		OD.Discount as 'Discount',
+		Convert(Decimal(10,2),Round((OD.UnitPrice*OD.Quantity*(1-OD.Discount)),2)) as 'Total Cost'
+from NorthwindTraders.dbo.[Order Details] as OD
+inner join NorthwindTraders.dbo.Products as P
+	on OD.ProductID=P.ProductID
+	where OrderID=@Order
+go
+
+exec GetOrderDetails 11011
+go
