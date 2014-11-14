3e49c874746833b7ed30971165bac9c1355c4b3b
Brandon Foote <brandon.foote@gmail.com>
2014-11-14 14:06:43 -0700

    asdf
    asdf

--- CMPE2400BrandonFooteICA14.sql
+++ CMPE2400BrandonFooteICA14.sql
@@ -17,7 +17,7 @@ create proc NewPublisher
 @newState as char(2),
 @newCountry as varchar(30)
 as
-if @newID is not null 
+if @newID is not null or @newID='1756' OR @newID='1622' OR @newID='0877' OR @newID='0736' OR @newID='1389' OR @newID like '99[0-9][0-9]' 
 begin
 	Insert into bfoote1_pubs.dbo.publishers
 		(pub_id,pub_name,city,state,country)
@@ -27,9 +27,10 @@ begin
 end
 go
 
---exec NewPublisher 9917,'Me','Edmonton','AB','Canada'
+--exec NewPublisher null,'Brandon','Edmonton','AB','Canada'
 --go
 
+--select * from bfoote1_pubs.dbo.publishers
 --Question 2
 
 if exists
@@ -52,7 +53,8 @@ create proc NewAuthor
 @AuthorZip as char(5),
 @AuthorContract as bit
 as
-if @AuthorID is not null --and @AuthorID like'[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]'
+if @AuthorID is not null and @AuthorID like'[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]' and @AuthorLName is not null
+	and @AuthorFName is not null and @AuthorPhone is not null and @AuthorContract is not null
 begin
 insert into bfoote1_pubs.dbo.authors
 	(au_id,au_lname,au_fname,phone,address,city,state,zip,contract)
@@ -61,8 +63,10 @@ insert into bfoote1_pubs.dbo.authors
 end
 go
 
---exec NewAuthor '165-85-1289','FO','BR','123-123-1234','559','Edm','AB','13245',1
+--exec NewAuthor '175-85-1289','FO','BR','123-123-1234','559','Edm','AB','13245',1
 --go
+
+--select * from bfoote1_pubs.dbo.authors
 
 --Question 3
 
@@ -84,18 +88,78 @@ create proc NewHire
 @EmpJobLvl as tinyint,
 @EmpPubID as char(4),
 @EmpHireDate as datetime,
-@Error as varChar(50) output
+@Error as varChar(50)
 as
-if(@EmpID is not null)
-begin
-insert into bfoote1_pubs.dbo.employee
-	(emp_id,fname,minit,lname,job_id,job_lvl,pub_id,hire_date)
-values
-	(@EmpID,@EmpFName,@EmpMinit,@EmpLName,@EmpJobID,@EmpJobLvl,@EmpPubID,@EmpHireDate)
-end
+	begin transaction
+		insert into bfoote1_pubs.dbo.employee
+			(emp_id,fname,minit,lname,job_id,job_lvl,pub_id,hire_date)
+		values
+			(@EmpID,@EmpFName,@EmpMinit,@EmpLName,@EmpJobID,@EmpJobLvl,@EmpPubID,@EmpHireDate)
+
+		if @@ERROR <> 0
+		begin
+			rollback transaction
+			print @Error
+			return 1
+		end
+	commit transaction
 go
 
-exec NewHire 'AMB12345M','BR','J','FO',2,123,'1234','1991-08-27','Error'
+exec NewHire 'AMB14345M','BR','J','FO',2,201,'9999','1991-08-27','Error'
 
 select * from bfoote1_pubs.dbo.employee
+go
+
+--Question 4
+
+if exists
+	(
+		select 	[name]
+		from 	sysobjects
+		where 	[name] = 'newTitle'
+	)
+	drop proc NewTitle
+	go
+
+create proc newTitle
+@TitleID as varchar(6),
+@Title as varchar(80),
+@TitleType as char(12),
+@TitlePubID as char(4),
+@TitlePrice as money,
+@TitleAdvance as money,
+@TitleRoyalty as integer,
+@TitleYTDSales as integer,
+@TitleNotes as varchar(200),
+@TitlePubDate as datetime,
+@TitleAuID as varchar(11),
+@TitleAuOrd as tinyint,
+@TitleAuthorRoyaltyPer as integer
+as
+	begin transaction
+		insert into bfoote1_pubs.dbo.titles
+			(title_id,title,type,pub_id,price,advance,royalty,ytd_sales,notes,pubdate)
+		values
+			(@TitleID,@Title,@TitleType,@TitlePubID,@TitlePrice,@TitleAdvance,@TitleRoyalty,@TitleYTDSales,@TitleNotes,@TitlePubDate)
+		
+		if @@ERROR <> 0
+		begin
+			rollback transaction
+			print 'First Insert Failed'
+			return 1
+		end
+		insert into bfoote1_pubs.dbo.titleauthor	
+			(au_id,title_id,au_ord,royaltyper)
+		values
+			(@TitleAuID,@TitleID,@TitleAuOrd,@TitleAuthorRoyaltyPer)
+		if @@ERROR <> 0
+		begin
+			rollback transaction
+			return 2
+			print 'Second Failed'
+		end
+	commit transaction
+go
+
+exec newTitle  '123456','Annoying Course','business','1389',19.99,5000.00,10,123,'I hate this class','1991-08-27','175-85-1289',12,12
 go
